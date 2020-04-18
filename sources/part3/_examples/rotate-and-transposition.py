acce1e55963a5f5363e1f80e80e2c34899a20204
Armalor <armalor@gmail.com>
2020-04-18 19:23:35 +0300

    Поворот и перенос.

--- sources/part3/_examples/rotate-and-transposition.py
+++ sources/part3/_examples/rotate-and-transposition.py
@@ -15,7 +15,7 @@ start_point = np.array([0, 0])
 
 # End coordinate, here (250, 250)
 # represents the bottom right corner of image
-end_point = np.array([0, 100])
+end_point = np.array([0, 500])
 
 color = 255
 
@@ -30,16 +30,22 @@ mask = cv2.inRange(img, np.array([255]), np.array([255]))
 def rotation(point: np.array, teta):
     rad = math.radians(teta)
     print(point, rad)
+
     matrix = np.array([
-        [math.cos(rad), -math.sin(rad)],
-        [math.sin(rad), math.cos(rad)]
+        [math.cos(rad), math.sin(rad)],
+        [-math.sin(rad), math.cos(rad)]
     ])
 
-    x1 = point[0] * math.cos(rad) + point[1]*math.sin(rad)
-    y1 = point[1] * math.cos(rad) - point[0]*math.sin(rad)
-    ret = np.array([x1,y1]).astype(np.uint8)
-    print(ret)
-    return ret
+    x1 = point[0] * np.cos(rad) + point[1] * np.sin(rad)
+    y1 = point[1] * np.cos(rad) - point[0] * np.sin(rad)
+
+    ret = np.array([x1, y1]).astype(np.uint8)
+    print('direct:', ret)
+
+    ret = matrix.dot(point)
+    print('matrix:', ret.astype(np.uint8))
+
+    return ret.astype(np.uint8)
 
 
 start_point1 = start_point
@@ -48,7 +54,6 @@ end_point1 = rotation(end_point, 45)
 start_point2 = start_point
 end_point2 = rotation(end_point, 90)
 
-print(end_point, end_point1,end_point2)
 
 while True:
 
