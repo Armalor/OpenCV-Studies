ca50891189d998b00a5f1b44475818ea3a08e20f
Armalor <armalor@gmail.com>
2020-04-19 11:35:35 +0300

    Все оказалось довольно просто...

--- sources/part3/_examples/rotate-and-transposition.py
+++ sources/part3/_examples/rotate-and-transposition.py
@@ -20,46 +20,38 @@ end_point = np.array([0, 500])
 color = 255
 
 # Line thickness of 9 px
-thickness = 3
+thickness = 2
 
 
 
 mask = cv2.inRange(img, np.array([255]), np.array([255]))
 
 
-def rotation(point: np.array, teta):
-    rad = math.radians(teta)
-    print(point, rad)
+def rotation(point: np.array, teta_deg):
+    teta = math.radians(teta_deg)
+
 
     matrix = np.array([
-        [math.cos(rad), math.sin(rad)],
-        [-math.sin(rad), math.cos(rad)]
+        [math.cos(teta), math.sin(teta)],
+        [-math.sin(teta), math.cos(teta)]
     ])
 
-    x1 = point[0] * np.cos(rad) + point[1] * np.sin(rad)
-    y1 = point[1] * np.cos(rad) - point[0] * np.sin(rad)
-
-    ret = np.array([x1, y1]).astype(np.uint8)
-    print('direct:', ret)
-
     ret = matrix.dot(point)
-    print('matrix:', ret.astype(np.uint8))
-
-    return ret.astype(np.uint8)
+    return ret.astype(np.uint16)
 
 
 start_point1 = start_point
-end_point1 = rotation(end_point, 45)
+end_point1 = rotation(end_point, 22.5)
 
 start_point2 = start_point
-end_point2 = rotation(end_point, 90)
+end_point2 = rotation(end_point, 45)
 
 
 while True:
 
 
     img = cv2.line(img, tuple(start_point), tuple(end_point), color, thickness)
-    img = cv2.line(img, tuple(start_point1), tuple(end_point1), 200, thickness)
+    img = cv2.line(img, tuple(start_point1), tuple(end_point1), 150, thickness)
     img = cv2.line(img, tuple(start_point2), tuple(end_point2), 100, thickness)
 
     res = cv2.bitwise_or(img, background, mask=None)
